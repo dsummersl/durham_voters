edb553c5c57c70db1bf1a360f4f42464fe3acda5
Dane Summers <dsummersl@yahoo.com>
2017-05-15 08:55:27 -0400

    Improves address cleaning & remove non-active voters.

--- arcgis/test_utils.py
+++ arcgis/test_utils.py
@@ -6,16 +6,25 @@ def test_create_address_fields():
     sbe_data = pd.DataFrame({
         'mail_addr1': pd.Series([
             '3523 N ROXBORO ST # 3H',
-            '111 E CHANNING AVE',
-            '801 LORAINE AVE',
-            '707 EAST HUDSON AVE',
-            '53 MOXY HUDSON AVE'
-            # '116 � E EDGEWOOD DR'
+            '111 E CHANNING AVE  ',
+            '801  LORAINE AVE',
+            '  707   EAST   HUDSON AVE',
+            '53 MOXY HUDSON AVE #7F',
+            '116� E EDGEWOOD DR'
         ])
     })
     create_address_fields(sbe_data, 'mail_addr1')
-    assert list(sbe_data['clean_street_number']) == ['3523', '111', '801', '707', '53']
-    assert list(sbe_data['clean_street_name']) == ['ROXBORO', 'CHANNING', 'LORAINE', 'HUDSON', 'MOXY HUDSON']
-    assert list(sbe_data['clean_street_directional']) == ['N', 'E', '', 'E', '']
-    assert list(sbe_data['clean_street_type']) == ['ST', 'AVE', 'AVE', 'AVE', 'AVE']
-    assert list(sbe_data['clean_street_apartment']) == ['3H', '', '', '', '']
+    assert list(sbe_data['clean_street_number']) == ['3523', '111', '801', '707', '53', '116']
+    assert list(sbe_data['clean_street_name']) == ['ROXBORO', 'CHANNING', 'LORAINE', 'HUDSON', 'MOXY HUDSON', 'EDGEWOOD']
+    assert list(sbe_data['clean_street_directional']) == ['N', 'E', '', 'E', '', 'E']
+    assert list(sbe_data['clean_street_type']) == ['ST', 'AVE', 'AVE', 'AVE', 'AVE', 'DR']
+    assert list(sbe_data['clean_street_apartment']) == ['3H', '', '', '', '7F', '']
+
+    assert list(sbe_data['clean_address']) == [
+        '3523 N ROXBORO ST 3H',
+        '111 E CHANNING AVE',
+        '801 LORAINE AVE',
+        '707 E HUDSON AVE',
+        '53 MOXY HUDSON AVE 7F',
+        '116 E EDGEWOOD DR'
+    ]
--- arcgis/utils.py
+++ arcgis/utils.py
@@ -1,19 +1,24 @@
 def create_address_fields(sbe_data, column):
     """ Cleanup SBE mail_addr1 field from sbe_data. """
-    sbe_data['clean_street_number'] = sbe_data[column].str.replace(r'^([0-9]+) .*$', r'\1')
-    apartments = sbe_data[column].str.contains(' # ').fillna(False)
+    sbe_data['clean_street_number'] = sbe_data[column].str.replace(r'^\s*([0-9]+)\S*\s+.*$', r'\1')
+    apartments = sbe_data[column].str.contains(r'\s+#').fillna(False)
     sbe_data['clean_street_name'] = ''
-    sbe_data.loc[apartments, 'clean_street_name'] = sbe_data.loc[apartments, column].str.replace(r'^[0-9]+ (.+) \S+ # .*$', r'\1')
-    sbe_data.loc[~apartments, 'clean_street_name'] = sbe_data.loc[~apartments, column].str.replace(r'^[0-9]+ (.+) \S+$', r'\1')
-    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^EAST (\S+)$', r'E \1')
-    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^WEST (\S+)$', r'W \1')
-    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^NORTH (\S+)$', r'N \1')
-    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^SOUTH (\S+)$', r'S \1')
+    sbe_data.loc[apartments, 'clean_street_name'] = sbe_data.loc[apartments, column].str.replace(r'^\s*[0-9]+\S*\s+(.+)\s+\S+\s+#.*$', r'\1')
+    sbe_data.loc[~apartments, 'clean_street_name'] = sbe_data.loc[~apartments, column].str.replace(r'^\s*[0-9]+\S*\s+(.+)\s+\S+.*$', r'\1')
+    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^EAST\s+(\S+)$', r'E \1')
+    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^WEST\s+(\S+)$', r'W \1')
+    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^NORTH\s+(\S+)$', r'N \1')
+    sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^SOUTH\s+(\S+)$', r'S \1')
     sbe_data['clean_street_directional'] = ''
     sbe_data['clean_street_directional'] = sbe_data['clean_street_name'].str.replace(r'^(N|S|W|E|NE|NW|SE|SW)? ?\S+.*$', r'\1')
     sbe_data['clean_street_name'] = sbe_data['clean_street_name'].str.replace(r'^(N|S|W|E|NE|NW|SE|SW) ', '')
     sbe_data['clean_street_type'] = ''
-    sbe_data.loc[apartments, 'clean_street_type'] = sbe_data.loc[apartments, column].str.replace(r'^[0-9]+ .* (\S+) # .*$', r'\1')
-    sbe_data.loc[~apartments, 'clean_street_type'] = sbe_data.loc[~apartments, column].str.replace(r'^[0-9]+ .* (\S+)$', r'\1')
+    sbe_data.loc[apartments, 'clean_street_type'] = sbe_data.loc[apartments, column].str.replace(r'^\s*[0-9]+\S*\s+.*\s+(\S+)\s+#.*$', r'\1')
+    sbe_data.loc[~apartments, 'clean_street_type'] = sbe_data.loc[~apartments, column].str.replace(r'^\s*[0-9]+\S*\s+.*\s+(\S+).*$', r'\1')
     sbe_data['clean_street_apartment'] = ''
-    sbe_data.loc[apartments, 'clean_street_apartment'] = sbe_data.loc[apartments, column].str.replace(r'^[0-9]+ .* # (.*)$', r'\1')
+    sbe_data.loc[apartments, 'clean_street_apartment'] = sbe_data.loc[apartments, column].str.replace(r'^\s*[0-9]+\S*\s+.*\s+#\s*(.*)$', r'\1')
+    sbe_data['clean_address'] = '' + sbe_data['clean_street_number']
+    sbe_data['clean_address'] = sbe_data['clean_address'] + sbe_data['clean_street_directional'].str.replace(r'^(\S+)$', r' \1')
+    sbe_data['clean_address'] = sbe_data['clean_address'] + ' ' + sbe_data['clean_street_name']
+    sbe_data['clean_address'] = sbe_data['clean_address'] + ' ' + sbe_data['clean_street_type']
+    sbe_data['clean_address'] = sbe_data['clean_address'] + sbe_data['clean_street_apartment'].str.replace(r'^(\S+)$', r' \1')
