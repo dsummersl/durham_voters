63d6db352fc3e58ffef40c5b36f7409fcb756155
Dane Summers <dsummersl@yahoo.com>
2017-06-18 17:30:17 -0400

    Cleans up output; bulk pandas append makes it faster.

--- find_apartments.py
+++ find_apartments.py
@@ -3,51 +3,63 @@ import pandas as pd
 from tqdm import tqdm
 
 lots = pd.read_csv('durham_parcels.csv')
-# lots = pd.read_csv('head.csv')
 lots = lots.set_index(['clean_address_y'], drop=False)
 
 searched = []
+new_addresses = []
 
 for i, row in tqdm(lots.iterrows(), total=lots.shape[0]):
     address = row['clean_full_street_y']
     if isinstance(address, float):
         address = row['clean_full_street_x']
+    # Have we already searched this streed for addresses/apartments?
     if address in searched:
         continue
     searched.append(address)
-    # Search for all apartments on the full street name (to minimize HTTP calls):
+    # Search for all addresses/apartments on the street to minimize HTTP calls:
     apartments = utils.find_unique_apartments(address)
     if len(apartments) == 0:
         continue
-    new_addresses = []
     for a in apartments:
         if a['clean_street_apartment'] == '':
-            # Don't include the 'lot address' if there are apartments
+            # Don't include the 'lot address' if it is not an apartment
             continue
-        not_in_lots = a['clean_address'] not in lots.index
-        if not not_in_lots:
+        in_lots = a['clean_address'] in lots.index
+        # Don't add the address if it already exists in our dataset:
+        if in_lots:
             continue
-        new_addresses.append([
-            row.PARCEL_ID,
-            '',  # row.voter_reg_num,
-            row.clean_number_address,
-            row.clean_full_street_x,
-            row.clean_full_street_y,
-            '',  # row.clean_address_x,
-            a['clean_address'],
-            row.lat,
-            row.long,
-            '',  # row.first_name,
-            '',  # row.middle_name,
-            '',  # row.last_name,
-            '',  # row.birth_age,
-            '',  # row.voter_status_desc,
-            ''   # row.party_cd
-        ])
-    if len(new_addresses) > 0:
-        lots = lots.append(new_addresses)
-        print('{}: {} new addresses'.format(row['clean_number_address'], len(new_addresses)))
+        # Found a new address that doesn't exist in our dataset, with no known
+        # voter associated with it:
+        new_addresses.append({
+            'PARCEL_ID': row.PARCEL_ID,
+            'voter_reg_num': '',
+            'clean_full_street_x': row.clean_full_street_x,
+            'clean_full_street_y': row.clean_full_street_y,
+            'clean_address_x': '',
+            'clean_address_y': a['clean_address'],
+            'lat': row.lat,
+            'long': row.long,
+            'first_name': '',
+            'middle_name': '',
+            'last_name': '',
+            'birth_age': '',
+            'voter_status_desc': '',
+            'party_cd': ''
+        })
 
-lots.to_csv('durham_addresses.csv', index=False)
-# TODO if there is a clean_number_address with a lot/long, when adding new
-# missing lots, use the lat/long from other parcels (if there is one).
+if len(new_addresses) > 0:
+    lots = lots.append(new_addresses, ignore_index=True)
+    # print('{}: {} new addresses'.format(address, len(new_addresses)))
+
+lots.PARCEL_ID = lots.PARCEL_ID.fillna(0).astype(int)
+lots.voter_reg_num = pd.to_numeric(lots.voter_reg_num).fillna(0).astype(int)
+lots = lots.fillna('')
+no_lot_address = lots['clean_address_x'].str.contains(r'^$').fillna(False)
+lots['address'] = '' + lots['clean_address_x']
+lots.loc[no_lot_address, 'address'] = '' + lots.loc[no_lot_address, 'clean_address_y']
+lots[[
+    'PARCEL_ID', 'voter_reg_num',
+    'address', 'lat', 'long',
+    'first_name', 'middle_name', 'last_name', 'birth_age',
+    'voter_status_desc', 'party_cd'
+]].to_csv('durham_addresses.csv', index=False)
